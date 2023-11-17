a93c327368092ba49d3c3efa7f248d83f848a94d
Anish-Palakurthi <anish.palakurthi@utexas.edu>
2023-11-16 20:37:49 -0600

    AP samples are aligned and reordered

---
+++ helpers/reverse_slice_order.py
@@ -0,0 +1,34 @@
+import os
+
+def reverse_file_naming(folder_path):
+    # First, rename all files to a temporary unique name
+    temp_rename_map = {filename: f"temp_{filename}" for filename in os.listdir(folder_path) if filename.endswith('.png')}
+    for old_name, temp_name in temp_rename_map.items():
+        os.rename(os.path.join(folder_path, old_name), os.path.join(folder_path, temp_name))
+
+    # Determine the highest slice number
+    max_slice_number = 0
+    for temp_name in temp_rename_map.values():
+        slice_number = int(temp_name.split('_slice_')[1].split('.png')[0])
+        max_slice_number = max(max_slice_number, slice_number)
+
+    # Rename files to their new names with reversed numbering
+    for temp_name in temp_rename_map.values():
+        slice_number = int(temp_name.split('_slice_')[1].split('.png')[0])
+        new_slice_number = max_slice_number - slice_number + 1
+        new_filename = temp_name.replace(f'_slice_{slice_number}', f'_slice_{new_slice_number}').replace('temp_', '')
+        os.rename(os.path.join(folder_path, temp_name), os.path.join(folder_path, new_filename))
+        print(f'Renamed {temp_name} to {new_filename}')
+
+def process_all_subfolders(top_folder):
+    # Loop through each subfolder in the top folder
+    for subfolder in os.listdir(top_folder):
+        subfolder_path = os.path.join(top_folder, subfolder)
+        # Check if it is a directory
+        if os.path.isdir(subfolder_path):
+            print(f"Processing folder: {subfolder_path}")
+            reverse_file_naming(subfolder_path)
+
+# Example usage
+top_folder = '/Users/anishpalakurthi/Desktop/flair_infarct_estimation/FLAIR_Labels/rotate_results'  # Replace with your top-level folder path
+process_all_subfolders(top_folder)
--- helpers/rotate_png.py
+++ helpers/rotate_png.py
@@ -1,39 +1,37 @@
 from PIL import Image
 import os
 
-def rotate_images_in_folder(folder_path, output_folder):
-    # Create output directory if it doesn't exist
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
+def rotate_images_180(folder_path):
     # Loop through all files in the folder
     for filename in os.listdir(folder_path):
         # Check if the file is an image
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+        if filename.lower().endswith('.png'):
             try:
                 # Open the image
                 img_path = os.path.join(folder_path, filename)
                 with Image.open(img_path) as img:
-                    # Rotate the image
-                    rotated_img = img.rotate(90, expand=True)
+                    # Rotate the image 180 degrees
+                    rotated_img = img.rotate(180)
 
-                    # Save the rotated image to the output folder
-                    rotated_img.save(os.path.join(output_folder, filename))
+                    # Save the rotated image
+                    rotated_img.save(os.path.join(folder_path, filename))
                     print(f"Rotated and saved: {filename}")
 
             except Exception as e:
-                print(f"Error processing {filename}: {e}")
+                print(f"Error rotating {filename}: {e}")
 
-def process_all_subfolders(top_folder, output_base_folder):
+def process_all_subfolders(top_folder):
     # Loop through each subfolder in the top folder
     for subfolder in os.listdir(top_folder):
         subfolder_path = os.path.join(top_folder, subfolder)
         # Check if it is a directory
         if os.path.isdir(subfolder_path):
-            # Create a corresponding output directory for the subfolder
-            output_dir = os.path.join(output_base_folder, subfolder)
-            rotate_images_in_folder(subfolder_path, output_dir)
+            print(f"Processing folder: {subfolder_path}")
+            rotate_images_180(subfolder_path)
 
 # Example usage
-process_all_subfolders('/Users/anishpalakurthi/Desktop/flair_infarct_estimation/FLAIR_Labels/KESHAV_pngs', 
-                       '/Users/anishpalakurthi/Desktop/flair_infarct_estimation/FLAIR_Labels/KESHAV_rotated_pngs')
+process_all_subfolders('/Users/anishpalakurthi/Desktop/flair_infarct_estimation/FLAIR_Labels/rotate_results')
+
+
+
+
